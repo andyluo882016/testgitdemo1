7d8ac038e71f5350d00009ce96f2312ced7ab8a2
andyluo882016 <andylu882016>
2022-02-26 14:16:55 -0800

    commit v0 @2/26 new branchshow2

--- MyFirstDemos/src/main/java/com/app/AppShows.java
+++ MyFirstDemos/src/main/java/com/app/AppShows.java
@@ -3,6 +3,6 @@ package com.app;
 public class AppShows {//
 
     public static void main(String[] args){
-        System.out.println("The First Test line1");
+         System.out.println("The First Test line1");
     }
 }
