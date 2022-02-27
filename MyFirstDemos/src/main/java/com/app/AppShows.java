1435a03cb11c4c4206301a7a844f1274d652c519
andyluo882016 <andylu882016>
2022-02-26 22:37:58 -0800

    commit v0 @2/26 new branchshow2

--- MyFirstDemos/src/main/java/com/app/AppShows.java
+++ MyFirstDemos/src/main/java/com/app/AppShows.java
@@ -5,4 +5,7 @@ public class AppShows {//
     public static void main(String[] args){
          System.out.println("The First Test line1");
     }
+    public int add(int a, int b){
+        return a+b;
+    }
 }
