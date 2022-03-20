e28be52efdb2ddfd64cf1bf3cdb216a942938b38
andyluo882016 <andyluo882016@gmail.com>
2022-03-20 14:03:39 -0700

    first commit User @3/20

--- MyTestGitDemo2/src/main/com/app/domain/User.java
+++ MyTestGitDemo2/src/main/com/app/domain/User.java
@@ -1,2 +1,12 @@
 public class User {
+
+    private String id;
+    private String name;
+    private Integer age;
+
+    public User(String id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
